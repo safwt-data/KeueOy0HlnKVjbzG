d346e9776f7f7cff1b96c2c2c33b3d23a6487624
Safwt <80886399+safwt-data@users.noreply.github.com>
2026-09-15 15:41:48 +0200

    .

---
+++ project_2/modeling/dataset.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import logging
+
+
+def load_data():
+    logging.info("Loading dataset")
+    df = pd.read_csv("Data/term-deposit-marketing-2020.csv")
+    logging.info("Dataset loaded successfully")
+    return df
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
+    df = load_data()
